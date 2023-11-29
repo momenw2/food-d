f047ec2e6544335cf76a2844c88da75533f1945d
momenw2 <momen2550@icloud.com>
2023-11-30 00:31:12 +0700

    backend

--- frontend/src/services/foodService.js
+++ frontend/src/services/foodService.js
@@ -1,16 +1,24 @@
-import { sample_foods, sample_tags } from "../data";
-export const getAll = async () => sample_foods;
+import axios from "axios";
+export const getAll = async () => {
+    const { data } = await axios.get('api/foods');
+    return data;
+};
 
-export const search = async (searchTerm) => 
-sample_foods.filter(item => 
-    item.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
-);
+export const search = async searchTerm => 
+{
+    const { data } = await axios.get('/api/foods/search/' + searchTerm);
+    return data;
+}
 
-export const getAllTags = async () => sample_tags;
+export const getAllTags = async () => {
+    const { data } = await axios.get('/api/foods/tags');
+    return data;
+};
 
 export const getAllByTag = async tag => {
     if (tag === 'All') return getAll();
-    return sample_foods.filter(item => item.tags?.includes(tag));
+    const { data } = await axios.get('/api/foods/tag/' + tag);
+    return data;
     };
 
     // export const getById = async foodId => {
@@ -23,9 +31,10 @@ export const getAllByTag = async tag => {
     // }
 
         export const getById = async (foodId) => {
-            const food = sample_foods.find((item) => item.id === foodId);
-            if (!food) {
-            throw new Error(`Food with ID ${foodId} not found`);
-            }
-            return food;
+            const { data } = await axios.get('/api/foods/' + foodId);
+            return data;           
+             // if (!food) {
+            // throw new Error(`Food with ID ${foodId} not found`);
+            // }
+            // return food;
         };
